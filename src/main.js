259544de87bdc75563575c49e825eddb7981639b
neromaxor <141302051+neromaxor@users.noreply.github.com>
2024-01-10 21:16:19 +0200

    update

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
 
 
-const URL_API = "https://pixabay.com/api/";
+const BASE_URL = "https://pixabay.com/api/";
 const API_KEY = "41731122-7275ff6ee1092a0a4f098cc9b";
 
 const form = document.querySelector(".form");
@@ -10,3 +10,28 @@ const loader = document.querySelector(".loader");
 const photoContainer = document.querySelector(".photo-container");
 
 
+let gallery;
+
+
+const getBaseUrl = () => {
+    
+    const url = new URL(BASE_URL);
+    url.searchParams.append("key", API_KEY);
+    url.searchParams.append(("image_type", "photo"));
+    url.searchParams.append("orientation", "horizontal");
+    url.searchParams.append("safesearch", "true");
+
+
+
+    return url;
+};
+
+
+
+
+
+// form.addEventListener("submit", (event) => {
+//     event.preventDefault();
+    
+
+// })
