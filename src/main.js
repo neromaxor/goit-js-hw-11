3a0c164840a3e7e3ea96226c61f0b304b34fe16d
neromaxor <141302051+neromaxor@users.noreply.github.com>
2024-01-10 20:02:04 +0200

    update

---
+++ src/main.js
@@ -0,0 +1,12 @@
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
+
+const URL_API = "https://pixabay.com/api/";
+const API_KEY = "41731122-7275ff6ee1092a0a4f098cc9b";
+
+const form = document.querySelector(".form");
+const loader = document.querySelector(".loader");
+const photoContainer = document.querySelector(".photo-container");
+
+
